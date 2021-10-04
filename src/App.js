420824a5c923ef48ba40043ce08d9839fd7b821c
yyogesh <yogeshggn1@gmail.com>
2021-10-04 08:14:16 +0530

    React styled-component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import styled, { css } from 'styled-components';
+
+
+const Wrapper = styled.section`
+ padding: 4rem;
+ margin: 2em;
+ width: 200px;
+ background-color: ${({ bgColor }) => bgColor ? bgColor : 'papayawhip'};
+ ${({ is3D }) => is3D && css`
+    background-color: yellow;
+    box-shadow: 1rem 1rem black;
+ `}
+`
+
+const Title = styled.h1`
+ font-size: 1.5rem;
+ text-align: center;
+ color: palevioletred;
+`
+
+const CircleWrapper = styled(Wrapper)`
+border-radius:100%;
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper>
+        <Title>Hello Title</Title>
+      </Wrapper>
+      <Wrapper bgColor={"silver"}>
+        <Title>Hello Title</Title>
+      </Wrapper>
+      <Wrapper bgColor={"silver"} is3D={true}>
+        <Title>Hello Title</Title>
+      </Wrapper>
+      <CircleWrapper bgColor={"silver"}>
+        <Title>Hello Title</Title>
+      </CircleWrapper>
     </div>
   );
 }
